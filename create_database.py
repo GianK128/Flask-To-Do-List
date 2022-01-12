5a3caa1642f3dc14f65bc7277630a495f5ff8fc0
GianK128 <66274694+GianK128@users.noreply.github.com>
2022-01-12 01:03:06 -0300

    Agregar usuarios al crear BBDD

--- create_database.py
+++ create_database.py
@@ -1,3 +1,18 @@
 from todo_list import db
+from todo_list.models import User
 
 db.create_all()
+
+admin = User(username="admin", email="admin@example.com", password="password")
+user = User(username="user", email="user@example.com", password="password")
+
+db.session.add_all([admin, user])
+db.session.commit()
+
+# _user = User.query.get(1)
+# print(_user.relations)
+# print(_user.subrelations)
+
+# _user = User.query.get(2)
+# print(_user.relations)
+# print(_user.subrelations)
