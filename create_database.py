e8a7eff800eeb8836cb31eee8b2b9de7c4b224e6
GianK128 <66274694+GianK128@users.noreply.github.com>
2022-01-13 00:39:51 -0300

    Cambiar archivo de crear BBDD

--- create_database.py
+++ create_database.py
@@ -3,16 +3,16 @@ from todo_list.models import User
 
 db.create_all()
 
-admin = User(username="admin", email="admin@example.com", password="password")
-user = User(username="user", email="user@example.com", password="password")
+# admin = User(username="admin", email="admin@example.com", password="password")
+# user = User(username="user", email="user@example.com", password="password")
 
-db.session.add_all([admin, user])
-db.session.commit()
+# db.session.add_all([admin, user])
+# db.session.commit()
 
-# _user = User.query.get(1)
-# print(_user.relations)
-# print(_user.subrelations)
+_user = User.query.get(1)
+print(_user.friends)
+print(_user.friend_requests)
 
-# _user = User.query.get(2)
-# print(_user.relations)
-# print(_user.subrelations)
+_user = User.query.get(2)
+print(_user.friends)
+print(_user.friend_requests)
