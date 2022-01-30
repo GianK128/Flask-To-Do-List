e7e2f8808dd1414179a3de4003f626bb1f50de21
GianK128 <66274694+GianK128@users.noreply.github.com>
2022-01-30 15:48:59 -0300

    feat: Agregar envio de mail de verificacion cuenta

--- todo_list/auth.py
+++ todo_list/auth.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from todo_list.models import User
 from todo_list import db
 
 from todo_list.forms import LoginForm, RegisterForm
 from todo_list.token import gen_confirm_token, confirm_token
+from todo_list.email import send_email
 
 auth = Blueprint('auth', __name__)
 
@@ -22,9 +23,16 @@ def sign_up():
         db.session.commit()
 
         token = gen_confirm_token(new_user.email)
+        url = url_for('auth.confirm_email', token=token, _external=True)
+        html = render_template(
+            'activate_account.html', 
+            username=new_user.username,
+            confirm_url=url
+        )
+        send_email(new_user.email, "Verificación de correo para su cuenta de To Do List", html)
 
         login_user(new_user)
-        flash("Se ha registrado exitosamente.", category='success')
+        flash("Se ha registrado exitosamente. Chequee su correo para verificar su cuenta", category='success')
         return redirect(url_for('list.my_lists', user=new_user.username))
     if form.errors != {}:
         for err_msg in form.errors.values():
@@ -47,9 +55,9 @@ def login():
 
 @login_required
 @auth.route('confirm')
-def confirm_email(token):
+def confirm_email():
     try:
-        email = confirm_token(token)
+        email = confirm_token(request.args.get('token'))
     except:
         flash('El link de confirmación es invalido o expiró. Intente nuevamente.', 'danger')
         return redirect(url_for('routes.home'))
