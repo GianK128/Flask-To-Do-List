5f3e5387e2c079e2400c035b3b9f6c311257e2ad
GianK128 <66274694+GianK128@users.noreply.github.com>
2022-02-03 17:51:29 -0300

    fix: Sortear correctamente por dia los logs

--- todo_list/routes.py
+++ todo_list/routes.py
@@ -2,16 +2,17 @@ from flask import render_template, Blueprint
 from flask_login import current_user
 from sqlalchemy import desc
 from todo_list.models import User, ActivityLog, friends_table
+from collections import defaultdict
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 @routes.route('/home')
 def home():
-    activities = []
+    days = {}
     if current_user.is_authenticated:
         # Query (parecida):
-        # SELECT * FROM activity_log INNER JOIN user, friend_relationship where (first_user_id == 1 and second_user_id == user_id and second_user_id == user.id) or (first_user_id == 1 and activity_log.user_id == 1 and user.id == activity_log.user_id) ORDER BY activity_log.date DESC
+        # SELECT * FROM activity_log INNER JOIN user, friend_relationship where (first_user_id == 1 and second_user_id == user_id and second_user_id == user.id) or (first_user_id == 1 and activity_log.user_id == 1 and user.id == activity_log.user_id) ORDER BY activity_log.date DESC, activity_log.id DESC
         activities = ActivityLog.query\
             .join(User)\
             .filter(
@@ -19,5 +20,10 @@ def home():
                 (ActivityLog.user_id == current_user.id)
             )\
             .order_by(desc(ActivityLog.date))\
+            .order_by(desc(ActivityLog.id))\
             .all()
-    return render_template('home.html', activities = activities)
+        
+        days = defaultdict(list)
+        for log in activities:
+            days[log.date.toordinal()].append(log)
+    return render_template('home.html', days = days)
